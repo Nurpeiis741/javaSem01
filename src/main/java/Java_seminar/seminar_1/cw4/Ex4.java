08b7ca9f46b7f8e7684d0390c49016ed430e2049
Nurpeiis741 <52882719+Nurpeiis741@users.noreply.github.com>
2023-05-07 14:43:34 +0600

    Update Ex4.java

--- src/main/java/Java_seminar/seminar_1/cw4/Ex4.java
+++ src/main/java/Java_seminar/seminar_1/cw4/Ex4.java
@@ -1,3 +1,5 @@
+//hw1
+
 package Java_seminar.seminar_1.cw4;
 
 public class Ex4 {
@@ -15,3 +17,34 @@ public class Ex4 {
     }
 
 }
+
+//HW2
+
+import java.util.Scanner;
+
+public class hw2 {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+        int[] nums = new int[] {1,2,1,2,-1,1,3,1,3,-1,0};
+        int n = scanner.nextInt();
+        int summ = 0;
+
+
+        for (int i = 0; nums > i; i++) {
+            int b = scanner.nextInt();
+            if (b<0) {
+                summ+=i-1;
+            }
+            if (b == 0){
+                break;
+            }
+
+        }
+        System.out.println(summ);
+        scanner.close();
+    }
+
+}
+
+
+
