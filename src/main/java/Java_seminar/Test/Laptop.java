c988fac08f6f6399555ddf6e6fbeaa5a312bd711
sevensay <120299542+sevensay@users.noreply.github.com>
2023-05-24 15:08:17 +0600

    AtestTest

---
+++ src/main/java/Java_seminar/Test/Laptop.java
@@ -0,0 +1,48 @@
+package Java_seminar.Test;
+
+public class Laptop {
+    public String brand;
+    public int ram;
+    public int hdd;
+    public String os;
+    public String color;
+
+    public Laptop(String brand, int ram, int hdd, String os, String color) {
+        this.brand = brand;
+        this.ram = ram;
+        this.hdd = hdd;
+        this.os = os;
+        this.color = color;
+    }
+
+    public String getBrand() {
+        return brand;
+    }
+
+    public int getRam() {
+        return ram;
+    }
+
+    public int getHdd() {
+        return hdd;
+    }
+
+    public String getOs() {
+        return os;
+    }
+
+    public String getColor() {
+        return color;
+    }
+
+    @Override
+    public String toString() {
+        return "Laptop{" +
+                "brand='" + brand + '\'' +
+                ", ram=" + ram +
+                ", hdd=" + hdd +
+                ", os='" + os + '\'' +
+                ", color='" + color + '\'' +
+                '}';
+    }
+}
